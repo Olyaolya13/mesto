66e95e692f3a38f91437f72beeb032d9d02c7b72
Olga Pankrashina <olyaolya2713@yandex.ru>
2023-02-14 19:43:40 +0300

    feat: add opportuniyun for change name and occupation

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,26 @@
 const popupOpen = document.querySelector('.popup'); //фон окна попап
-if (!popupOpen) {
-  throw new Error('No popupOpen'); // проверяем переменную
-}
 const popupOpenContainer = document.querySelector('.popup__container'); // окно попапа
-if (!popupOpenContainer) {
-  throw new Error('No popupOpenContainer'); // проверяем переменную
-}
-const buttonEditPopup = document.querySelector('.content__button-edit'); // переменная для редактирования и открытия окна
+const buttonEditPopup = document.querySelector('.content__button-edit'); // переменная кнопки для редактирования и открытия окна
+const buttonClosePopup = document.querySelector('.popup__close-icon'); // переменная кнопки для закрытия окна попапа (крестик)
 
-if (!buttonEditPopup) {
-  throw new Error('No buttonEditPopup'); // проверяем переменную
-}
-const buttonClosePopup = document.querySelector('.popup__close-icon'); // переменная для закрытия окна попапа (крестик)
-if (!buttonClosePopup) {
-  throw new Error('No buttonClosePopup'); // проверяем переменную
-}
+//--------------------------------------------------------------
+
+let userName = 'Жак-Ив Кусто'; // переменная ФИО на странице
+let userJobTitle = 'Исследователь'; // переменная должность на странице
+
+const userNameElementWindow = document.querySelector('.content__title');
+userNameElementWindow.textContent = userName; // textContent получаем данные на странице (ФИО)
+
+const userJobTitleElementWindow = document.querySelector('.content__subtitle');
+userJobTitleElementWindow.textContent = userJobTitle; // textContent получаем данные на странице (Должность)
+
+//--------------------------------------------------------------
+const userNameEditPopup = document.querySelector('#popup__user-name'); // переменная для ФИО в попапе
+const userJobTitleEditPopup = document.querySelector('#popup__user-occupation'); // переменная для должности в попапе
 
 buttonEditPopup.addEventListener('click', openPopupFunction); // действие клика при окрытии редактирования попапа
-
 function openPopupFunction() {
-  //функция для кнокпки редактирования
+  //функция для открытия кнокпки редактирования
   popupOpen.classList.add('popup_opened'); //добавляем класс для фона попапа для открытия
   popupOpenContainer.classList.add('popup_opened'); //добавляем класс для самого окна попапа для открытия
 }
@@ -29,17 +30,19 @@ function closePopupFunction() {
   popupOpen.classList.remove('popup_opened'); //удаляем класс для фона попапа для открытия, получая закрытие фона попапа
   popupOpenContainer.classList.remove('popup_opened'); //удаляем класс для самого попапа для открытия, получая закрытие окна попапа
 }
+//-------------------------------------------------------
 
-let userName = 'Жак-Ив Кусто'; // переменная ФИО на странице
-let userJobTitle = 'Исследователь океана'; // переменная должность на странице
+const buttonPopupSave = document.querySelector('.popup__button-save'); // переменная для кнопки сохранения в попап
+buttonPopupSave.addEventListener('click', closePopupFunction); // функция закрытия для кнопки сохранения
 
-const userNameElementWindow = document.querySelector('.content__title');
-userNameElementWindow.textContent = userName; // textContent получаем данные на странице (ФИО)
+userNameEditPopup.addEventListener('input', function handleFormSubmit(event) {
+  event.preventDefault();
+  const value = event.target.value;
+  userNameElementWindow.textContent = value;
+});
 
-const userJobTitleElemenWindow = document.querySelector('.content__subtitle');
-userJobTitleElemenWindow.textContent = userJobTitle; // textContent получаем данные на странице (Должность)
-
-const userNameEditPopup = document.querySelector('#popup__user-name');
-userNameEditPopup.value = userName; // value получаем данные в попап ФИО
-const userJobTitleEditPopup = document.querySelector('#popup__user-occupation');
-userJobTitleEditPopup.value = userJobTitle; // value получаем данные в попап Должность
+userJobTitleEditPopup.addEventListener('input', function handleFormSubmit(event) {
+  event.preventDefault();
+  const value = event.target.value;
+  userJobTitleElementWindow.textContent = value;
+});
