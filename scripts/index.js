45b6b5ab54046939da10f280d8f7569615e8a359
Olga Pankrashina <olyaolya2713@yandex.ru>
2023-02-17 16:08:16 +0300

    fix: add new const in js, add function for submit, add function for button edit, delete over const

--- scripts/index.js
+++ scripts/index.js
@@ -1,51 +1,31 @@
-const popupOpen = document.querySelector('.popup'); //фон окна попап
-const popupOpenContainer = document.querySelector('.popup__container'); // окно попапа
-const buttonEditPopup = document.querySelector('.content__button-edit'); // переменная кнопки для редактирования и открытия окна
-const buttonClosePopup = document.querySelector('.popup__close-icon'); // переменная кнопки для закрытия окна попапа (крестик)
-
-//--------------------------------------------------------------
-
-let userName = 'Жак-Ив Кусто'; // переменная ФИО на странице
-let userJobTitle = 'Исследователь'; // переменная должность на странице
-
-const userNameElementWindow = document.querySelector('.content__title');
-userNameElementWindow.textContent = userName; // textContent получаем данные на странице (ФИО)
-
-const userJobTitleElementWindow = document.querySelector('.content__subtitle');
-userJobTitleElementWindow.textContent = userJobTitle; // textContent получаем данные на странице (Должность)
-
-//--------------------------------------------------------------
-const userNameEditPopup = document.querySelector('#popup__user-name'); // переменная для ФИО в попапе
-const userJobTitleEditPopup = document.querySelector('#popup__user-occupation'); // переменная для должности в попапе
+const popupOpen = document.querySelector('.popup'); //переменная окна попапа
+const buttonEditPopup = document.querySelector('.content__button-edit'); // переменная кнопки для редактирования и открытия попапа
+const buttonClosePopup = document.querySelector('.popup__close-icon'); // переменная кнопки для закрытия попапа (крестик)
+const userNameElementWindow = document.querySelector('.content__title'); // переменная ФИО в окне
+const userJobTitleElementWindow = document.querySelector('.content__subtitle'); // переменная Должность в окне
+const nameInput = document.querySelector('#popup__user-name'); // переменная ФИО в попапе
+const jobInput = document.querySelector('#popup__user-occupation'); // переменная Должность в попапе
 
 buttonEditPopup.addEventListener('click', openPopupFunction); // действие клика при окрытии редактирования попапа
 function openPopupFunction() {
-  //функция для открытия кнокпки редактирования
-  popupOpen.classList.add('popup_opened'); //добавляем класс для фона попапа для открытия
-  popupOpenContainer.classList.add('popup_opened'); //добавляем класс для самого окна попапа для открытия
+  //функция для открытия кнокпки редактирования со значениями окна
+  nameInput.value = userNameElementWindow.textContent; // значения в попапе value берем с окна textContent
+  jobInput.value = userJobTitleElementWindow.textContent;
+  popupOpen.classList.add('popup_opened');
 }
 
 buttonClosePopup.addEventListener('click', closePopupFunction); // действие клика при закрытии редактирования попапа
 function closePopupFunction() {
-  popupOpen.classList.remove('popup_opened'); //удаляем класс для фона попапа для открытия, получая закрытие фона попапа
-  popupOpenContainer.classList.remove('popup_opened'); //удаляем класс для самого попапа для открытия, получая закрытие окна попапа
+  popupOpen.classList.remove('popup_opened'); //удаляем класс для  попапа для открытия, получая закрытие попапа
 }
-//-------------------------------------------------------
 
-const buttonPopupSave = document.querySelector('.popup__button-save'); // переменная для кнопки сохранения в попап
-buttonPopupSave.addEventListener('click', closePopupFunction); // функция закрытия для кнопки сохранения
+const formElement = document.querySelector('.popup__form'); // переменная формы попапа
 
-userNameEditPopup.addEventListener('input', function handleFormSubmit(event) {
-  // функция редактирования для Фио
-
+formElement.addEventListener('submit', handleFormSubmit); // действие сохранения формы попапа
+function handleFormSubmit(event) {
+  //функция для сохранения значений с попапа и перенос в окно
   event.preventDefault();
-  const value = event.target.value;
-  userNameElementWindow.textContent = value;
-});
-
-userJobTitleEditPopup.addEventListener('input', function handleFormSubmit(event) {
-  // функция редактирования для Должности
-  event.preventDefault();
-  const value = event.target.value;
-  userJobTitleElementWindow.textContent = value;
-});
+  userNameElementWindow.textContent = nameInput.value;
+  userJobTitleElementWindow.textContent = jobInput.value;
+  closePopupFunction(); //вызвали повтороно функцию закрытия попапа
+}
